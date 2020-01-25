c2437e791d4f127c63fc33dcb10dec86dd80fa4c
Zalán Valkó <valzalan@gmail.com>
2020-01-25 19:13:24 +0100

    Add details activity and extract strings

---
+++ app/src/main/java/com/valzalan/weather/DetailsView.java
@@ -0,0 +1,15 @@
+package com.valzalan.weather;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailsView extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
